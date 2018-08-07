b56d0b6d76f2fa3aa9f512610b5d7734394483b7
Ruben Cancino <luxsoft.cancino@gmail.com>
2018-08-07 18:29:04 -0500

    ajustes

--- sx-compras-server/grails-app/services/sx/cxp/ContrareciboService.groovy
+++ sx-compras-server/grails-app/services/sx/cxp/ContrareciboService.groovy
@@ -2,6 +2,7 @@ package sx.cxp
 
 import grails.compiler.GrailsCompileStatic
 import grails.gorm.services.Service
+import sx.core.Folio
 import sx.core.LogUser
 
 
@@ -11,9 +12,22 @@ abstract class ContrareciboService implements LogUser {
 
     abstract Contrarecibo save( Contrarecibo recivo)
 
+    abstract void delete(Serializable id)
+
     Contrarecibo saveRecibo(Contrarecibo  recibo) {
+        if(!recibo.id) {
+            recibo.folio = nextFolio()
+        }
         logEntity(recibo)
         return save(recibo)
+    }
+
+    Long  nextFolio(){
+        Folio folio = Folio.findOrCreateWhere(entidad: 'CXP', serie: 'CONTRARECIBOS')
+        Long res = folio.folio + 1
+        folio.folio = res
+        folio.save flush: true
+        return res
     }
 
 
