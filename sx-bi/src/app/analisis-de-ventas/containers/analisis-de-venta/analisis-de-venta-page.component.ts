6c08701c56b3ef4e231cd6c542085eaa137b7a69
Ruben Cancino <luxsoft.cancino@gmail.com>
2018-10-18 19:32:31 -0500

    Analisis page initial

--- sx-bi/src/app/analisis-de-ventas/containers/analisis-de-venta/analisis-de-venta-page.component.ts
+++ sx-bi/src/app/analisis-de-ventas/containers/analisis-de-venta/analisis-de-venta-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { TdMediaService } from '@covalent/core';
 
 import { Store, select } from '@ngrx/store';
-import * as fromStore from '../../store';
+import * as fromStore from 'app/store';
 import * as fromAuth from 'app/auth/store';
 
 import { Observable } from 'rxjs';
@@ -15,7 +15,6 @@ import { User } from 'app/auth/models/user';
   styleUrls: ['./analisis-de-venta-page.component.scss']
 })
 export class AnalisisDeVentaPageComponent implements OnInit {
-  loading$: Observable<boolean>;
   user$: Observable<User>;
   api$: Observable<any>;
 
@@ -25,7 +24,6 @@ export class AnalisisDeVentaPageComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.loading$ = this.store.pipe(select(fromStore.getNotasLoading));
     this.user$ = this.store.pipe(select(fromAuth.getUser));
     this.api$ = this.store.pipe(select(fromAuth.getApiInfo));
   }
