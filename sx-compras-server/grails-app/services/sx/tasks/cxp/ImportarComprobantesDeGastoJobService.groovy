5e556c466414f9ab7dcb3d2c06649266586823b9
Ruben Cancino <luxsoft.cancino@gmail.com>
2018-11-22 09:13:17 -0600

    Ajuste a importador correr solo en tesoreria enviroment

--- sx-compras-server/grails-app/services/sx/tasks/cxp/ImportarComprobantesDeGastoJobService.groovy
+++ sx-compras-server/grails-app/services/sx/tasks/cxp/ImportarComprobantesDeGastoJobService.groovy
@@ -18,16 +18,16 @@ class ImportarComprobantesDeGastoJobService {
     @Scheduled(fixedDelay = 120000L, initialDelay = 120000L)
     void importarComprobantesDeCompras() {
 
-       if(Environment.current == Environment.DEVELOPMENT)
-            return
+        Environment.executeForEnvironment(Environment.getEnvironment('tesoreria'), {
+            log.info('Importando CFDIS de GASTOS  desde {} , {}', comprobanteFiscalService.gastosDir, new Date().format("dd/MM/yyyy hh:mm:ss"))
+            try{
+                int rows = comprobanteFiscalService.importacionLocal('GASTOS', true)
+                log.info('Comprobantes importados: {}', rows)
+            } catch (Exception ex) {
+                String message = ExceptionUtils.getRootCauseMessage(ex)
+                log.error(message)
+            }
+        })
 
-        log.info('Importando CFDIS de GASTOS  desde {} , {}', comprobanteFiscalService.gastosDir, new Date().format("dd/MM/yyyy hh:mm:ss"))
-        try{
-            int rows = comprobanteFiscalService.importacionLocal('GASTOS', true)
-            log.info('Comprobantes importados: {}', rows)
-        } catch (Exception ex) {
-            String message = ExceptionUtils.getRootCauseMessage(ex)
-            log.error(message)
-        }
     }
 }
