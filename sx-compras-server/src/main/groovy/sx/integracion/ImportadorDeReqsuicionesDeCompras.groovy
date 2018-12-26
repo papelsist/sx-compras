756d21be1694c7cb5bf400c3efb7b7c100b7c486
Ruben Cancino <rubencancino6@gmail.com>
2018-12-26 13:12:53 -0600

    Importacion especial de requisiciones y su egreso para enreo 2018

--- sx-compras-server/src/main/groovy/sx/integracion/ImportadorDeReqsuicionesDeCompras.groovy
+++ sx-compras-server/src/main/groovy/sx/integracion/ImportadorDeReqsuicionesDeCompras.groovy
@@ -6,7 +6,8 @@ import org.apache.commons.lang3.exception.ExceptionUtils
 import sx.core.Proveedor
 import sx.cxp.Pago
 import sx.cxp.PagoService
-import sx.cxp.RequisicionDeGastos
+import sx.cxp.RequisicionDeCompras
+
 import sx.tesoreria.Cheque
 import sx.tesoreria.CuentaDeBanco
 import sx.tesoreria.MovimientoDeCuenta
@@ -17,103 +18,115 @@ import java.sql.SQLException
 @Slf4j
 class ImportadorDeReqsuicionesDeCompras {
 
+    ImportadorDeReqsuicionesDeCompras importarEgresos(int ejercicio, int mes){
+        List<Map> rows = getRows("""
+        select * from sw_bcargoabono 
+        where origen = 'COMPRAS' 
+        and year(fecha) > ?  and month(fecha) = ? 
+        order by fecha asc
+        """, ejercicio, mes)
 
-    ImportadorDeReqsuicionesDeCompras importarRequisiciones(Periodo periodo) {
-        def select = """
-    	select c.tipo as gtipo, t.*,d.documento,pp.rfc as rfc2, c.proveedor_id as prov , pp.nombre as provNombre
-        from sw_trequisicion t 
-        join sw_trequisiciondet d on(t.requisicion_id = d.requisicion_id)
-        join sx_gas_facxreq2 g on(g.requisicionesdet_id = d.requisicionde_id)
-        join sw_facturas_gastos f on(g.factura_id = f.id)
-        join sw_gcompra c on(f.compra_id = c.compra_id)
-        join sw_gproveedor pp on(pp.proveedor_id = c.proveedor_id)
-        where origen = 'GASTOS' and pp.rfc is not null
-        and date(t.fecha) between ? and ? 
-        order by t.fecha """
-        def rows = getRows(select, periodo.fechaInicial, periodo.fechaFinal)
+        Sql db = getSql()
+
         rows.each { row ->
             try {
-                importarRegistro(row)
+                Long egresoId = row['cargoabono_id'] as Long
+                RequisicionDeCompras requisicion = findRequisicion(db, egresoId)
+                if(requisicion) {
+                    MovimientoDeCuenta egreso = findEgreso(row, requisicion.formaDePago)
+                    requisicion.egreso = egreso
+                    requisicion.pagada = egreso.fecha
+                    egreso.save failOnError: true, flush: true
+                    requisicion.save failOnError: true, flush: true
+                    if(egreso.formaDePago == 'CHEQUE') {
+                        Cheque cheque = findCheque(egreso, row)
+                        egreso.cheque = cheque
+                        cheque.save failOnError: true, flush: true
+                        egreso.save
+                    }
+                    log.info('Requisicion importada {}', requisicion)
+                }
+
             }catch(Exception ex) {
                 def message = ExceptionUtils.getRootCauseMessage(ex)
-                log.error('Error importando {} {}', row.requsicion_id, message)
+                log.error('Error importando {} {}', row ,message)
             }
         }
-        return this
-    }
 
-    ImportadorDeReqsuicionesDeCompras importarEgresos(Periodo periodo){
-        def c = RequisicionDeGastos.createCriteria()
-        def results = c {
-            between("fecha", periodo.fechaInicial, periodo.fechaFinal)
-            isNull("egreso")
-        }
-        def db = getSql()
-        results.each { RequisicionDeGastos it ->
-            try {
-                def row = findEgreso(db, it)
-                if(row){
-                    log.info('CargoAbono a importar: {}' , row.cargoabono_id)
-                    MovimientoDeCuenta egreso = importarEgreso(row, it)
-                    it.egreso = egreso
-                    it.pagada = egreso.fecha
-                    it.save flush: true
-                }
-            }catch (SQLException e){
-                def ex = ExceptionUtils.getRootCause(e)
-                def message = ExceptionUtils.getRootCauseMessage(e)
-                println 'Error buscando egreso ' + message
-            }
-
-        }
-        db.close()
-
-
-        return this
-    }
-
-    ImportadorDeReqsuicionesDeCompras importarCheques(Periodo periodo) {
-        def c = RequisicionDeGastos.createCriteria()
-        List<RequisicionDeGastos> results = c {
-            between("fecha", periodo.fechaInicial, periodo.fechaFinal)
-            isNotNull("egreso")
-            eq('formaDePago', 'CHEQUE')
-            egreso {
-                isNull('cheque')
-            }
-        }
-        Sql db = getSql()
-        results.each {
-            try {
-                def row = findEgreso(db, it)
-                MovimientoDeCuenta egreso = it.egreso
-                Cheque cheque = new Cheque()
-                cheque.cuenta = egreso.cuenta
-                cheque.nombre = egreso.afavor
-                cheque.fecha = egreso.fecha
-                cheque.folio = egreso.referencia.toLong()
-                cheque.importe = egreso.importe.abs()
-                cheque.egreso = egreso
-                cheque.impresion = row.impreso
-                cheque.cobrado = row.FECHACOBRADO
-
-                cheque.save failOnError: true, flush: true
-
-                egreso.cheque = cheque
-                egreso.save flush: true
-
-                log.info('Generando cheque: {}', it)
-            }catch(Exception ex) {
-                log.error('Error generando cheque {}' , ex.message)
-            }
-        }
         db.close()
         return this
+
+    }
+
+
+
+    RequisicionDeCompras findRequisicion(Sql db, Long id) {
+        String SELECT = """
+            select * from sw_trequisicion where cargoabono_id = ?
+        """
+        Map row = db.firstRow(SELECT,[id])
+
+        RequisicionDeCompras found = RequisicionDeCompras.where{sw2 == row.requisicion_id}.find()
+        if(found){
+            log.info('Req ya iportada: {} ', row.sw2)
+            return null
+        }
+        RequisicionDeCompras req = new RequisicionDeCompras()
+        req.folio = row.requisicion_id
+        req.proveedor = Proveedor.where{rfc == row.rfc2}.find()
+        req.nombre = row.afavor
+        req.moneda = row.MONEDA
+        req.tipoDeCambio = row.tc
+        req.fecha = row.fecha
+        req.fechaDePago = row.fechaDePago
+        req.formaDePago = row.FORMADEPAGO == 1 ? 'CHEQUE' : 'TRANSFERENCIA'
+        req.total = row.total
+        req.apagar = row.total
+        req.comentario = row.COMENTARIO
+        req.sw2 = req.folio
+        req.createUser = 'ADMIN'
+        req.updateUser = 'ADMIN'
+        return req
+    }
+
+
+    MovimientoDeCuenta findEgreso(Map row, String formaDePago) {
+        def cuenta = CuentaDeBanco.where{sw2 == row.cuenta_id}.find()
+        MovimientoDeCuenta egreso = new MovimientoDeCuenta()
+        egreso.tipo = 'COMPRA'
+        egreso.concepto = 'COMPRA'
+        egreso.importe = row.importe
+        egreso.fecha = row.fecha
+        egreso.moneda = Currency.getInstance(row.moneda)
+        egreso.tipoDeCambio = row.tc
+        egreso.comentario = row.comentario
+        egreso.formaDePago = formaDePago
+        egreso.referencia = row.referencia
+        egreso.afavor = row.afavor
+        egreso.cuenta = cuenta
+        egreso.createUser = 'admin'
+        egreso.updateUser = 'admin'
+        egreso.sw2 = row.cargoabono_id
+        return egreso
+    }
+
+
+    Cheque findCheque(MovimientoDeCuenta egreso, Map row) {
+        Cheque cheque = new Cheque()
+        cheque.cuenta = egreso.cuenta
+        cheque.nombre = egreso.afavor
+        cheque.fecha = egreso.fecha
+        cheque.folio = egreso.referencia.toLong()
+        cheque.importe = egreso.importe.abs()
+        cheque.egreso = egreso
+        cheque.impresion = row.impreso
+        cheque.cobrado = row.FECHACOBRADO
+        return  cheque
     }
 
     ImportadorDeReqsuicionesDeCompras importarPagosCxP(PagoService pagoService, Periodo periodo) {
         def c = RequisicionDeGastos.createCriteria()
-        List<RequisicionDeGastos> results = c {
+        List<RequisicionDeCompras> results = c {
             between("fecha", periodo.fechaInicial, periodo.fechaFinal)
         }
         results.each { req ->
@@ -131,73 +144,6 @@ class ImportadorDeReqsuicionesDeCompras {
         }
         return this
     }
-
-
-
-    def importarRegistro(def row) {
-        def found = RequisicionDeGastos.where{folio == row.requisicion_id}.find()
-        if(found){
-            log.info('Req ya iportada: {} ', row.requisicion_id)
-            return
-        }
-        Proveedor proveedor = Proveedor.where{rfc == row.rfc2}.find()
-        if(!proveedor) {
-            println "Registrando proveedor: ${row.provNombre?: row.afavor} RFC: ${row.rfc2}"
-            proveedor = new Proveedor(nombre: row.provNombre?: row.afavor, rfc: row.rfc2, tipo: 'GASTOS')
-            proveedor.clave = "GS${row.rfc2[0..-6]}"
-            proveedor.save failOnError: true, flush: true
-            log.info('Proveedor nuevo: {}', proveedor)
-        }
-        RequisicionDeGastos req = new RequisicionDeGastos()
-        req.folio = row.requisicion_id
-        req.proveedor = Proveedor.where{rfc == row.rfc2}.find()
-        req.nombre = row.afavor
-        req.moneda = row.MONEDA
-        req.tipoDeCambio = row.tc
-        req.fecha = row.fecha
-        req.fechaDePago = row.fechaDePago
-        req.formaDePago = row.FORMADEPAGO == 1 ? 'CHEQUE' : 'TRANSFERENCIA'
-        req.total = row.total
-        req.apagar = row.total
-        req.comentario = row.COMENTARIO
-        req.sw2 = req.folio
-        req.createUser = 'ADMIN'
-        req.updateUser = 'ADMIN'
-        req.save failOnError:true, flush:true
-        log.info("Requisicion importada {}", req.id)
-    }
-
-    def findEgreso(Sql db, RequisicionDeGastos row) {
-        def SQL = """
-    	select * from sw_bcargoabono 
-         where cargoabono_id = (select cargoabono_id from sw_trequisicion where requisicion_id = ?)
-        """
-        return db.firstRow(SQL,[row.folio])
-    }
-
-    MovimientoDeCuenta importarEgreso(def row, def requisicionDeGastos) {
-        def cuenta = CuentaDeBanco.where{sw2 == row.cuenta_id}.find()
-        MovimientoDeCuenta egreso = new MovimientoDeCuenta()
-        egreso.tipo = 'GASTO'
-        egreso.importe = row.importe
-        egreso.fecha = row.fecha
-        egreso.concepto = 'GASTO'
-        egreso.moneda = Currency.getInstance(row.moneda)
-        egreso.tipoDeCambio = row.tc
-        egreso.comentario = row.comentario
-        egreso.formaDePago = requisicionDeGastos.formaDePago
-        egreso.referencia = row.referencia
-        egreso.afavor = row.afavor
-        egreso.cuenta = cuenta
-        egreso.createUser = 'admin'
-        egreso.updateUser = 'admin'
-        egreso.sw2 = row.cargoabono_id
-        // requisicionDeGastos.save(flush: true)
-        // egreso.save failOnError: true, flush: true
-        return egreso
-    }
-
-
 
 
     def getRows(String sql, ...params) {
@@ -223,29 +169,5 @@ class ImportadorDeReqsuicionesDeCompras {
         Sql db = Sql.newInstance(dbUrl, user, password, driver)
         return db
     }
-
-
-    def updateCheques(){
-        List<RequisicionDeGastos> gastos = RequisicionDeGastos.where{formaDePago == 'CHEQUE'}.list()
-        Sql db = getSql()
-        gastos.each { gasto ->
-            if(gasto.egreso && gasto.egreso.cheque) {
-                Cheque cheque = gasto.egreso.cheque
-                try {
-                    def row = findEgreso(db, gasto)
-                    println 'ROW:' + row
-                    cheque.impresion = row.impreso
-                    cheque.cobrado = row.FECHACOBRADO
-                    cheque.save flush: true
-                    log.info('Cheque actualizado: {}', cheque)
-                }catch(Exception ex) {
-                    log.error('Error generando cheque {}' , ex.message)
-                }
-            }
-        }
-        db.close()
-        return this
-    }
-
 
 }
