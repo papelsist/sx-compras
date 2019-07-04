01de577e4891c2cf6747a8a834eb7ee3a4ac1bd0
Luis <lquintanillab@gmail.com>
2019-07-04 13:56:02 -0500

    poliza depositos tesoreria

--- sx-compras-server/src/main/groovy/sx/contabilidad/diario/DepositosTesoreriaProc.groovy
+++ sx-compras-server/src/main/groovy/sx/contabilidad/diario/DepositosTesoreriaProc.groovy
@@ -4,6 +4,8 @@ import groovy.util.logging.Slf4j
 import org.springframework.stereotype.Component
 import sx.contabilidad.*
 import sx.utils.Periodo
+import sx.tesoreria.MovimientoDeTesoreria
+import sx.tesoreria.MovimientoDeCuenta
 
 @Slf4j
 @Component
@@ -23,7 +25,7 @@ class DepositosTesoreriaProc implements  ProcesadorDePoliza, AsientoBuilder{
 
     @Override
     def generarAsientos(Poliza poliza, Map params) {
-            
+            procesarDepositos(poliza)
     }
 
 
@@ -31,18 +33,27 @@ class DepositosTesoreriaProc implements  ProcesadorDePoliza, AsientoBuilder{
         def movimientosTes = MovimientoDeTesoreria.executeQuery("from MovimientoDeTesoreria where fecha= ? and concepto in ('DEPOSITO_DEUDOR','DEVOLUCION_NOMINA','DEP_ACUENTA_PRESTAMO','DEVOLUCION_ASEGURADORA')",[poliza.fecha])
 
         movimientosTes.each{mov ->
-            switch(mov.concepto) {
-                case 'DEPOSITO_DEUDOR':
-                    
-                break
-                case 'DEVOLUCION_NOMINA':
-                    
-                break
 
+            String ctaBanco= "102-${mov.movimiento.moneda.currencyCode == 'MXN' ? '0001': '0002'}-${mov.movimiento.cuenta.subCuentaOperativa}-0000"
+            Map row = [
+                    asiento: mov.concepto,
+                    referencia: mov.comentario,
+                    referencia2: mov.comentario,
+                    origen: mov.id,
+                    documento: mov.id,
+                    documentoTipo: 'TES',
+                    documentoFecha: mov.fecha,
+                    sucursal: 'OFICINAS',
+                    montoTotal: mov.importe,
+                    moneda: mov.movimiento.moneda, 
+                ] 
+            String desc = generarDescripcion(row) 
+                    poliza.addToPartidas(mapRow(ctaBanco,desc,row,mov.importe)) 
+                    poliza.addToPartidas(mapRow(mov.cuentaContable,desc,row,0.00,mov.importe))       
             }
         }
 
-   }
+ 
 
  String generarDescripcion(Map row) {
         if(row.tc > 1.0) {
@@ -51,34 +62,27 @@ class DepositosTesoreriaProc implements  ProcesadorDePoliza, AsientoBuilder{
         return "F:${row.documento} (${row.documentoFecha}) "
     }
 
-
-
-    PolizaDet mapRow(String cuentaClave, row, def debe = 0.0, def haber = 0.0) {
-
+    PolizaDet mapRow(String cuentaClave, String descripcion, Map row, def debe = 0.0, def haber = 0.0) {
         CuentaContable cuenta = buscarCuenta(cuentaClave)
-        String descripcion = ""
         PolizaDet det = new PolizaDet(
-                
+                cuenta: cuenta,
+                concepto: cuenta.descripcion,
+                descripcion: descripcion,
+                asiento: row.asiento,
+                referencia: row.referencia2,
+                referencia2: row.referencia2,
+                origen: row.origen,
+                entidad: 'MovimientoDeTesoreria',
+                documento: row.documento,
+                documentoTipo: 'TES',
+                documentoFecha: row.fecha_doc,
+                sucursal: 'OFICINAS',
+                debe: debe.abs(),
+                haber: haber.abs()
         )
-
+        // Datos del complemento
+        asignarComprobanteNacional(det, row)
+        // asignarComplementoDePago(det, row)
         return det
     }
-
-
-    PolizaDet mapRow() {
-
-        CuentaContable cuenta = buscarCuenta(cuentaClave)
-
-        String descripcion = ""
-
-
-        PolizaDet det = new PolizaDet(
-
-        )
-
-        return det
-    }
-
-
-
 }
