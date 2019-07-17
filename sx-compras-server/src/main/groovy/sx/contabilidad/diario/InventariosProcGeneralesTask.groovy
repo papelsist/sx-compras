5641b5b689464c90419320f30433b402c631e4d8
Luis <lquintanillab@gmail.com>
2019-07-17 15:05:48 -0500

    Update InventariosProcGeneralesTask.groovy

--- sx-compras-server/src/main/groovy/sx/contabilidad/diario/InventariosProcGeneralesTask.groovy
+++ sx-compras-server/src/main/groovy/sx/contabilidad/diario/InventariosProcGeneralesTask.groovy
@@ -26,8 +26,6 @@ class InventariosProcGeneralesTask implements  AsientoBuilder {
         log.info("Generando asientos para inventarios generales")
         String sql = getSelect()
                 .replaceAll("@FECHA", toSqlDate(poliza.fecha))
-        
-        println sql
 
         List rows = getAllRows(sql, [])
         rows.each { row ->
@@ -40,12 +38,14 @@ class InventariosProcGeneralesTask implements  AsientoBuilder {
                         row,
                         0.0,
                         row.costo))
-                poliza.addToPartidas(mapRow(
+                if(row.referencia2 != '06 TRASLADOS'){
+                    poliza.addToPartidas(mapRow(
                         poliza,
                         "115-0001-${row.suc.toString().padLeft(4,'0')}-0000",
                         descripcion,
                         row,
                         row.costo))
+                } 
             }else {
                 poliza.addToPartidas(mapRow(
                         poliza,
@@ -53,16 +53,19 @@ class InventariosProcGeneralesTask implements  AsientoBuilder {
                         descripcion,
                         row,
                         row.costo))
-                poliza.addToPartidas(mapRow(
-                        poliza,
-                        "115-0001-${row.suc.toString().padLeft(4,'0')}-0000",
-                        descripcion,
-                        row,
-                        0.0,
-                        row.costo))
+                if(row.referencia2 != '06 TRASLADOS'){
+                    poliza.addToPartidas(mapRow(
+                            poliza,
+                            "115-0001-${row.suc.toString().padLeft(4,'0')}-0000",
+                            descripcion,
+                            row,
+                            0.0,
+                            row.costo))
+                }
             }
         }
     }
+
 
     @Override
     String generarDescripcion(Map row) {
@@ -82,7 +85,7 @@ class InventariosProcGeneralesTask implements  AsientoBuilder {
                 descripcion: descripcion,
                 asiento: row.asiento,
                 referencia: ref,
-                referencia2: ref,
+                referencia2: row.referencia2.substring(3),
                 origen: "${ejercicio}/${mes}",
                 entidad: "",
                 documento: '',
@@ -99,24 +102,26 @@ class InventariosProcGeneralesTask implements  AsientoBuilder {
     String getSelect() {
 
         String res = """
-        SELECT 
-        sum(x.costo) costo,                  
+               SELECT 
+        round(sum(x.costo),2) costo,                  
         x.sucursal,
         x.suc,        
         x.asiento,
         x.proveedor,
         x.grupo referencia2,        
         x.cta_contable        
-        FROM (
-        select 'FLETES PROVEEDOR' AS asiento,'FLT' TIPO  
-        ,'02 COMPRAS' AS grupo,S.clave suc,S.NOMBRE sucursal,0 as kilos,0 as saldo
-        ,ROUND(SUM(I.CANTIDAD/(case when p.unidad ='MIL' then 1000 else 1 end) * i.gasto),2) as costo,x.nombre proveedor
-        ,concat('115-0004-',(SELECT o.cuenta_operativa FROM cuenta_operativa_proveedor o where o.proveedor_id=x.id ),'-0000') as cta_contable,0 costo_final
+        FROM (        	   
+        SELECT 'FLETES PROVEEDOR' ASIENTO,'FLT' TIPO,'02 COMPRAS' AS grupo,s.sw2,S.clave SUC,S.NOMBRE SUCURSAL,P.CLAVE,P.DESCRIPCION 
+        ,0 as kilos,0 as saldo,   ( (I.CANTIDAD/(case when p.unidad ='MIL' then 1000 else 1 end) * i.gasto) ) as COSTO
+        ,(select x.nombre from recepcion_de_compra_det d join recepcion_de_compra r on(d.recepcion_id=r.id) join proveedor x on(r.proveedor_id=x.id) where d.inventario_id=i.id  ) PROVEEDOR                
+        ,concat('115-0004-',(SELECT o.cuenta_operativa FROM recepcion_de_compra_det d   join recepcion_de_compra r on(d.recepcion_id=r.id) join proveedor x on(r.proveedor_id=x.id) join cuenta_operativa_proveedor o on(o.proveedor_id=x.id) where d.inventario_id=i.id ),'-0000') CTA_CONTABLE
         from inventario I  join producto p on(p.id=i.producto_id) JOIN sucursal s on(i.sucursal_id=s.id)
-         join recepcion_de_compra_det d on(d.inventario_id=i.id) join recepcion_de_compra r on(d.recepcion_id=r.id) join proveedor x on(r.proveedor_id=x.id)
-        where I.TIPO='COM' AND I.GASTO>0 and YEAR(I.FECHA)=YEAR('@FECHA') AND MONTH(I.FECHA)=MONTH('@FECHA') AND I.CLAVE LIKE '%' group by 5,9
-        UNION
-        select (CASE WHEN i.TIPO IN('COM') AND I.COSTO=0 THEN 'COMPRAS_SNA'
+        where p.inventariable is true and YEAR(I.FECHA)=YEAR('@FECHA') AND MONTH(I.FECHA)=MONTH('@FECHA') AND I.CLAVE LIKE '%'
+        AND I.TIPO IN('COM') AND I.GASTO>0
+        	union
+        select a.ASIENTO,a.TIPO,a.GRUPO,a.sw2,a.SUC,a.SUCURSAL,a.CLAVE,a.DESCRIPCION,SUM(a.KILOS) KILOS,SUM(a.SALDO) SALDO,SUM(a.COSTO) COSTO,a.PROVEEDOR,a.CTA_CONTABLE 
+        from (        
+        SELECT (CASE WHEN i.TIPO IN('COM') AND I.COSTO=0 THEN 'COMPRAS_SNA'
         WHEN i.TIPO IN('COM') AND I.COSTO>0 THEN 'COMPRAS'
         WHEN i.TIPO IN('DEC') THEN 'DEVOLUCIONES DE COMPRAS'
         WHEN i.TIPO IN('FAC') THEN 'COSTO DE VENTA'
@@ -126,21 +131,21 @@ class InventariosProcGeneralesTask implements  AsientoBuilder {
         WHEN i.TIPO IN('TRS','REC') THEN 'TRANSFORMACIONES'
         WHEN i.TIPO IN('TPS') THEN 'TRASLADOS_SALIDA' 
         WHEN i.TIPO IN('TPE') THEN 'TRASLADOS_ENTRADA' 
-        WHEN i.TIPO IN('CIS') AND (SELECT d.tipocis FROM movimiento_de_almacen m join movimiento_de_almacen_det d on(d.movimiento_de_almacen_id=m.id) where d.inventario_id=i.id)='PAPELERIA' THEN 'PAPELERIA Y COPIAS'
-        WHEN i.TIPO IN('CIS') AND (SELECT d.tipocis FROM movimiento_de_almacen m join movimiento_de_almacen_det d on(d.movimiento_de_almacen_id=m.id) where d.inventario_id=i.id)='MATERIAL_EMPAQUE' THEN 'MATERIAL DE EMPAQUE'
-        WHEN i.TIPO IN('CIS') AND (SELECT d.tipocis FROM movimiento_de_almacen m join movimiento_de_almacen_det d on(d.movimiento_de_almacen_id=m.id) where d.inventario_id=i.id)='PUBLICIDAD_PROPAGANDA' THEN 'PUBLICIDAD PROPAGANDA'
-        WHEN i.TIPO IN('CIS') AND (SELECT d.tipocis FROM movimiento_de_almacen m join movimiento_de_almacen_det d on(d.movimiento_de_almacen_id=m.id) where d.inventario_id=i.id)='NO_DEDUSIBLE' THEN 'NO DEDUCIBLE'
+        WHEN i.TIPO IN('CIS') AND (SELECT d.tipocis FROM movimiento_de_almacen m join movimiento_de_almacen_det d on(d.movimiento_de_almacen_id=m.id) where d.inventario_id=i.id)='PAPELERIA' THEN 'PAPELERIA Y COPIAS CIS OGST'
+        WHEN i.TIPO IN('CIS') AND (SELECT d.tipocis FROM movimiento_de_almacen m join movimiento_de_almacen_det d on(d.movimiento_de_almacen_id=m.id) where d.inventario_id=i.id)='MATERIAL_EMPAQUE' THEN 'MATERIAL DE EMPAQUE CIS OGST'
+        WHEN i.TIPO IN('CIS') AND (SELECT d.tipocis FROM movimiento_de_almacen m join movimiento_de_almacen_det d on(d.movimiento_de_almacen_id=m.id) where d.inventario_id=i.id)='PUBLICIDAD_PROPAGANDA' THEN 'PUBLICIDAD PROPAGANDA CIS OGST'
+        WHEN i.TIPO IN('CIS') AND (SELECT d.tipocis FROM movimiento_de_almacen m join movimiento_de_almacen_det d on(d.movimiento_de_almacen_id=m.id) where d.inventario_id=i.id)='NO_DEDUSIBLE' THEN 'NO DEDUCIBLE CIS OGST'
         ELSE TIPO END) ASIENTO
         ,(CASE WHEN i.TIPO IN('COM') AND I.COSTO=0 THEN 'SNA' WHEN i.TIPO IN('TRS','REC') AND I.CANTIDAD>0 THEN concat('E',SUBSTR(i.tipo,1,2)) WHEN i.TIPO IN('TRS','REC') AND I.CANTIDAD<0 THEN concat('S',SUBSTR(i.tipo,1,2)) ELSE TIPO END) AS TIPO
-        ,(CASE WHEN I.TIPO IN('COM') THEN '02 COMPRAS' WHEN I.TIPO IN('AJU','CIM','CIS','DEC','MER','RMC') THEN '03 GASTO' WHEN I.TIPO IN('VIR','OIM') THEN '04 PRODUCTO'
-         WHEN I.TIPO IN('REC','TRS') THEN '05 TRANSFORM' 
-         WHEN I.TIPO IN('TPS') THEN '06 TRASLADO_SALIDA'
-         WHEN I.TIPO IN('TPE') THEN '06 TRASLADO_ENTRADA'
-         WHEN I.TIPO IN('FAC','RMD') THEN '07 VENTAS' ELSE 'OTROS' END) AS GRUPO,S.clave SUC,S.NOMBRE SUCURSAL
-        ,ROUND(SUM(I.CANTIDAD/(case when p.unidad ='MIL' then 1000 else 1 end)*P.KILOS),3) as KILOS,ROUND(SUM(I.CANTIDAD/(case when p.unidad ='MIL' then 1000 else 1 end)),3) as SALDO
-        ,ROUND(SUM(I.CANTIDAD/(case when p.unidad ='MIL' then 1000 else 1 end) * (case when i.tipo='COM' and i.costo>0 then i.costo when i.TIPO IN('TRS','REC') AND I.CANTIDAD>0 then i.costo else I.COSTO_promedio end)),2) as COSTO
+        ,(CASE WHEN I.TIPO IN('COM') THEN '02 COMPRAS' WHEN I.TIPO IN('AJU','CIM','CIS','DEC','MER','RMC') AND I.CANTIDAD<0  THEN '03 GASTO' WHEN I.TIPO IN('AJU','CIM','VIR','OIM') AND I.CANTIDAD>0  THEN '04 PRODUCTO'
+         WHEN I.TIPO IN('REC','TRS') THEN '05 TRANSFORMACIONES' 
+         WHEN I.TIPO IN('TPS') THEN '06 TRASLADOS'
+         WHEN I.TIPO IN('TPE') THEN '06 TRASLADOS'
+         WHEN I.TIPO IN('FAC','RMD') THEN '07 VENTAS' ELSE 'OTROS' END) AS GRUPO,s.sw2,S.clave SUC,S.NOMBRE SUCURSAL,P.CLAVE,P.DESCRIPCION
+        ,ROUND(   (I.CANTIDAD/(case when p.unidad ='MIL' then 1000 else 1 end)*P.KILOS),3) as KILOS,ROUND(   (I.CANTIDAD/(case when p.unidad ='MIL' then 1000 else 1 end)),3) as SALDO
+        ,    (   (I.CANTIDAD/(case when p.unidad ='MIL' then 1000 else 1 end) * (case when i.tipo='COM' and i.costo>0 then i.costo when i.TIPO IN('TRS','REC') AND I.CANTIDAD>0 then i.costo else I.COSTO_promedio end)) ) as COSTO
         ,(CASE WHEN i.TIPO IN('COM') AND I.COSTO>0 THEN (select x.nombre from recepcion_de_compra_det d join recepcion_de_compra r on(d.recepcion_id=r.id) join proveedor x on(r.proveedor_id=x.id) where d.inventario_id=i.id  )
-        WHEN i.TIPO IN('DEC') AND I.costo_promedio>0 THEN (select x.nombre from devolucion_de_compra_det d join devolucion_de_compra r on(d.devolucion_de_compra_id=r.id) join proveedor x on(r.proveedor_id=x.id) where d.inventario_id=i.id  )
+         WHEN i.TIPO IN('DEC') AND I.costo_promedio>0 THEN (select x.nombre from devolucion_de_compra_det d join devolucion_de_compra r on(d.devolucion_de_compra_id=r.id) join proveedor x on(r.proveedor_id=x.id) where d.inventario_id=i.id  )
          ELSE '' END) AS PROVEEDOR
         ,(CASE WHEN i.TIPO IN('COM') AND I.COSTO=0 THEN CONCAT('115-0001-',(CASE WHEN S.CLAVE<10 THEN '000' ELSE '00' END),S.CLAVE,'-0000')
         WHEN i.TIPO IN('COM') AND I.COSTO>0 THEN CONCAT('115-',(select (case when x.cuenta_operativa in('0061','0038') then concat('0002-',x.cuenta_operativa) else concat('0003-',x.cuenta_operativa) end) from recepcion_de_compra_det d join recepcion_de_compra r on(d.recepcion_id=r.id) join cuenta_operativa_proveedor x on(r.proveedor_id=x.proveedor_id) where d.inventario_id=i.id  ),'-0000')
@@ -149,16 +154,17 @@ class InventariosProcGeneralesTask implements  AsientoBuilder {
         WHEN i.TIPO IN('RMD') THEN CONCAT('501-0001-',(CASE WHEN S.CLAVE<10 THEN '000' ELSE '00' END),S.CLAVE,'-0000')
         WHEN i.TIPO IN('AJU','CIM','MER','RMC','OIM','VIR') AND I.CANTIDAD>0 THEN '704-0005-0000-0000'
         WHEN i.TIPO IN('AJU','CIM','MER','RMC','OIM','VIR') AND I.CANTIDAD<0 THEN CONCAT('600-0031-',(CASE WHEN S.CLAVE<10 THEN '000' ELSE '00' END),S.CLAVE,'-0000')
-        WHEN i.TIPO IN('TRS','REC') THEN CASE WHEN ROUND(SUM(I.CANTIDAD/(case when p.unidad ='MIL' then 1000 else 1 end) * (case when i.tipo='COM' and i.costo>0 then i.costo when i.TIPO IN('TRS','REC') AND I.CANTIDAD>0 then i.costo else I.COSTO_promedio end)),2)>0 THEN '704-0005-0000-0000' ELSE '703-0001-0000-0000' END
+        WHEN i.TIPO IN('TRS','REC') THEN CASE WHEN  (   (I.CANTIDAD/(case when p.unidad ='MIL' then 1000 else 1 end) * (case when i.tipo='COM' and i.costo>0 then i.costo when i.TIPO IN('TRS','REC') AND I.CANTIDAD>0 then i.costo else I.COSTO_promedio end))  )>0 THEN '704-0005-0000-0000' ELSE '703-0001-0000-0000' END
         WHEN i.TIPO IN('TPS') THEN CONCAT('115-0001-',(CASE WHEN S.CLAVE<10 THEN '000' ELSE '00' END),S.CLAVE,'-0000') 
         WHEN i.TIPO IN('TPE') THEN CONCAT('115-0001-',(CASE WHEN S.CLAVE<10 THEN '000' ELSE '00' END),S.CLAVE,'-0000') 
         WHEN i.TIPO IN('CIS') AND (SELECT d.tipocis FROM movimiento_de_almacen m join movimiento_de_almacen_det d on(d.movimiento_de_almacen_id=m.id) where d.inventario_id=i.id)='PAPELERIA' THEN CONCAT('600-0032-',(CASE WHEN S.CLAVE<10 THEN '000' ELSE '00' END),S.CLAVE,'-0000')
         WHEN i.TIPO IN('CIS') AND (SELECT d.tipocis FROM movimiento_de_almacen m join movimiento_de_almacen_det d on(d.movimiento_de_almacen_id=m.id) where d.inventario_id=i.id)='MATERIAL_EMPAQUE' THEN CONCAT('600-0029-',(CASE WHEN S.CLAVE<10 THEN '000' ELSE '00' END),S.CLAVE,'-0000')
         WHEN i.TIPO IN('CIS') AND (SELECT d.tipocis FROM movimiento_de_almacen m join movimiento_de_almacen_det d on(d.movimiento_de_almacen_id=m.id) where d.inventario_id=i.id)='PUBLICIDAD_PROPAGANDA' THEN CONCAT('600-0033-',(CASE WHEN S.CLAVE<10 THEN '000' ELSE '00' END),S.CLAVE,'-0000')
-        WHEN i.TIPO IN('CIS') AND (SELECT d.tipocis FROM movimiento_de_almacen m join movimiento_de_almacen_det d on(d.movimiento_de_almacen_id=m.id) where d.inventario_id=i.id)='NO_DEDUSIBLE' THEN CONCAT('600-0031-',(CASE WHEN S.CLAVE<10 THEN '000' ELSE '00' END),S.CLAVE,'-0000')
-        ELSE TIPO END) CTA_CONTABLE,0 COSTO_FINAL
+        WHEN i.TIPO IN('CIS') AND (SELECT d.tipocis FROM movimiento_de_almacen m join movimiento_de_almacen_det d on(d.movimiento_de_almacen_id=m.id) where d.inventario_id=i.id)='NO_DEDUSIBLE' THEN CONCAT('600-0031-',(CASE WHEN S.CLAVE<10 THEN '000' ELSE '00' END),S.CLAVE,'-0000') ELSE TIPO END) CTA_CONTABLE
         from inventario I  join producto p on(p.id=i.producto_id) JOIN sucursal s on(i.sucursal_id=s.id)
-        where p.inventariable is true and YEAR(I.FECHA)=YEAR('@FECHA') AND MONTH(I.FECHA)=MONTH('@FECHA') AND I.CLAVE LIKE '%' group by 1,2,5,9 
+        where p.inventariable is true and YEAR(I.FECHA)=YEAR('@FECHA') AND MONTH(I.FECHA)=MONTH('@FECHA') AND I.CLAVE LIKE '%'
+        ) as a       
+        group by 1,2,6,7,12             
         ) as x
         group by 6,4,2,5 ,7
         """
