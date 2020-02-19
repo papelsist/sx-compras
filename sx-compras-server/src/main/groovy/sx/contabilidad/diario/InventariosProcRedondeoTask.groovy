abd9caa6d027fcf5f4d260c257117029a7a8a80c
Luis <lquintanillab@gmail.com>
2020-02-19 17:07:21 -0600

    conta

--- sx-compras-server/src/main/groovy/sx/contabilidad/diario/InventariosProcRedondeoTask.groovy
+++ sx-compras-server/src/main/groovy/sx/contabilidad/diario/InventariosProcRedondeoTask.groovy
@@ -28,7 +28,7 @@ class InventariosProcRedondeoTask implements  AsientoBuilder {
                 .replaceAll("@MES", poliza.mes.toString())
                 .replaceAll("@EJERCICIO", poliza.ejercicio.toString())
 
-        //println sql                   
+       // println sql                   
 
         List rows = getAllRows(sql, [])
         rows.each { row ->
@@ -94,19 +94,19 @@ class InventariosProcRedondeoTask implements  AsientoBuilder {
         FROM (
         select '02 COMPRAS' AS grupo,S.clave suc,S.NOMBRE sucursal,ROUND(SUM(I.CANTIDAD/(case when p.unidad ='MIL' then 1000 else 1 end) * i.gasto),2) as costo,0 costo_final
         from inventario I  join producto p on(p.id=i.producto_id) JOIN sucursal s on(i.sucursal_id=s.id)
-        where I.TIPO in('COM','MAQ') AND I.GASTO>0 and YEAR(I.FECHA)=((@EJERCICIO)) AND MONTH(I.FECHA)=(((@MES))) AND I.CLAVE LIKE '%' group by 3
+        where I.TIPO in('COM','MAQ') AND I.GASTO>0 and YEAR(I.FECHA)=((@EJERCICIO)) AND MONTH(I.FECHA)=(((@MES))) AND I.CLAVE LIKE '%' and s.activa is true group by 3
         UNION
         select (CASE WHEN I.TIPO IN('COM','DEC') THEN '02 COMPRAS' WHEN I.TIPO IN('AJU','CIM','CIS','MER','RMC') THEN '03 GASTO' WHEN I.TIPO IN('VIR','OIM') THEN '04 PRODUCTO'
                     WHEN I.TIPO IN('REC','TRS','MAQ') THEN '05 TRANSFORM' WHEN I.TIPO IN('TPE','TPS') THEN '06 TRASLADO' WHEN I.TIPO IN('FAC','RMD') THEN '07 VENTAS' ELSE 'OTROS' END) AS GRUPO,S.clave SUC,S.NOMBRE SUCURSAL
         ,ROUND(SUM(I.CANTIDAD/(case when p.unidad ='MIL' then 1000 else 1 end) * (case when i.tipo IN('COM','DEC') and i.costo>0 then i.costo when i.TIPO IN('TRS','REC','MAQ') AND I.CANTIDAD>0 then i.costo else I.COSTO_promedio end)),2) as COSTO,0 COSTO_FINAL
         from inventario I  join producto p on(p.id=i.producto_id) JOIN sucursal s on(i.sucursal_id=s.id)
-        where p.inventariable is true and YEAR(I.FECHA)=((@EJERCICIO)) AND MONTH(I.FECHA)=(((@MES))) AND I.CLAVE LIKE '%' group by 1,3 
+        where p.inventariable is true and YEAR(I.FECHA)=((@EJERCICIO)) AND MONTH(I.FECHA)=(((@MES))) AND I.CLAVE LIKE '%' and s.activa is true group by 1,3 
         UNION
         SELECT '01 A_INV' AS GRUPO,s.clave SUC,S.NOMBRE SUCURSAL,ROUND(SUM(I.EXISTENCIA_INICIAL/(case when p.unidad ='MIL' then 1000 else 1 end) * I.COSTO ),2) as COSTO,0 COSTO_FINAL
-        FROM existencia i join producto p on(p.id=i.producto_id) JOIN sucursal s on(i.sucursal_id=s.id) where p.inventariable is true and i.anio=((@EJERCICIO)) AND i.mes=(((@MES)))  AND I.CLAVE LIKE '%' group by 3
+        FROM existencia i join producto p on(p.id=i.producto_id) JOIN sucursal s on(i.sucursal_id=s.id) where p.inventariable is true and i.anio=((@EJERCICIO)) AND i.mes=(((@MES)))  AND I.CLAVE LIKE '%' and s.activa is true group by 3
         UNION
         SELECT '08 Z_INV' AS GRUPO,s.clave SUC,S.NOMBRE SUCURSAL,0 COSTO,ROUND(SUM(I.CANTIDAD/(case when p.unidad ='MIL' then 1000 else 1 end) * I.costo_promedio ),2) as COSTO_FINAL
-        FROM existencia i join producto p on(p.id=i.producto_id) JOIN sucursal s on(i.sucursal_id=s.id) where p.inventariable is true and i.anio=((@EJERCICIO)) AND i.mes=(((@MES)))  AND I.CLAVE LIKE '%' group by 3
+        FROM existencia i join producto p on(p.id=i.producto_id) JOIN sucursal s on(i.sucursal_id=s.id) where p.inventariable is true and i.anio=((@EJERCICIO)) AND i.mes=(((@MES)))  AND I.CLAVE LIKE '%' and s.activa is true group by 3
         ) as x group by 2
         """
         return res
